d1cfeb14c6b10324a448c3516e6738467429777b
Saoud <saoud@saoud.dev>
2021-03-24 20:02:32 -0700

    adding basic structre for API call
    Co-authored-by: Tyler Sinks <tyler.sinks@gmail.com>
    Co-authored-by: Collin Chapman <cchap14@gmail.com>
     Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

---
+++ src/js/exchangeRateAPI.js
@@ -0,0 +1,14 @@
+export default class ExchangeRate {
+  static getRate(currency) {
+    return fetch(`https://v6.exchangerate-api.com/v6/{process.env.API_KEY}/latest/USD`);
+    .then(function(response){
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .catch(function(error) {
+      return Error(error);
+    })
+  }
+}
